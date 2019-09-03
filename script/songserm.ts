58401a075f16cc2c9b6be208b07a0d04cd1d1f82
Chinnatip Taemkaeo <chinnatip@me.com>
2019-09-03 22:11:09 +0700

    fetch and bake to JSON

--- script/songserm.ts
+++ script/songserm.ts
@@ -1,1 +1,19 @@
-console.log('hello songserm')
+import axios from 'axios'
+const fs = require('fs')
+//
+const productURL = `https://sb.api.adv-tour.com/v1`
+const appKey = `44bf66fh896355a`
+const suffixKey = process.env.suffix
+const writeFile = process.env.path
+const axiosInstance = axios.create({
+  baseURL: productURL,
+  timeout: 5000,
+  headers: {
+    appKey: appKey
+  }
+})
+//
+const getAxios = async () => await axiosInstance.get(suffixKey)
+getAxios().then(resp =>
+  fs.writeFileSync(writeFile, JSON.stringify(resp.data.data, null, 2))
+)
