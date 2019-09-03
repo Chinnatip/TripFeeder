c6139f776b97e538a150ef47c1251952dbe31c60
Chinnatip Taemkaeo <chinnatip@me.com>
2019-09-03 21:14:11 +0700

    json 2 csv converter

---
+++ index.js
@@ -0,0 +1,3 @@
+// JSON CSV CLI
+// json2csv -i ./json/songserm_product_boat.json -c ./json/product_field.json -o ./csv/songserm_boat.csv
+// json2csv -i ./json/songserm_product_combo.json -c ./json/product_field.json -o ./csv/songserm_combo.csv
