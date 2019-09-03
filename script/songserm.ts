5b00a3361a99d84d12fb1918eca36e5fbac092c8
Chinnatip Taemkaeo <chinnatip@me.com>
2019-09-03 22:14:02 +0700

    tidy

--- script/songserm.ts
+++ script/songserm.ts
@@ -1,10 +1,13 @@
 import axios from 'axios'
 const fs = require('fs')
-//
+
+// Setup ProductUrl and AppKey
 const productURL = `https://sb.api.adv-tour.com/v1`
 const appKey = `44bf66fh896355a`
 const suffixKey = process.env.suffix
 const writeFile = process.env.path
+
+// Prepare AXIOS
 const axiosInstance = axios.create({
   baseURL: productURL,
   timeout: 5000,
@@ -12,7 +15,8 @@ const axiosInstance = axios.create({
     appKey: appKey
   }
 })
-//
+
+// Fetch Process and write file to JSON
 const getAxios = async () => await axiosInstance.get(suffixKey)
 getAxios().then(resp =>
   fs.writeFileSync(writeFile, JSON.stringify(resp.data.data, null, 2))
